70e5f51f8086a20be113ece3e35bfb27d3bfbd71
jadefogarty <jadefogarty@gmail.com>
2024-04-03 23:10:30 +0100

    adding another api call

--- controllers/date_controller.js
+++ controllers/date_controller.js
@@ -2,25 +2,38 @@ const data_model = require('../models/data_model');
 
 //GET /
 function date_index(req, res) {
-    res.render('index', {data: null});
+    res.render('index', { data: null });
 };
 
 //GET /date
 async function search_date(req, res) {
-    try{
+    try {
         const date = req.query.Date;
         console.log(date)
 
-        const apiData = await data_model.callApi(date);
-        console.log(apiData);
-        res.render('index',{data: apiData});
+        var dateArray = date.split("-");
+
+        var year = parseInt(dateArray[0]);
+        var month = parseInt(dateArray[1]);
+        var day = parseInt(dateArray[2]);
+
+        console.log("Year:", year);
+        console.log("Month:", month);
+        console.log("Day:", day);
+
+        const nasaApiData = await data_model.callNasaApi(date);
+        console.log(nasaApiData);
+
+        const mlApiData = await data_model.callMlApi(month, day);
+        console.log(mlApiData);
+        res.render('index', { nasaData: nasaApiData, mlData: mlApiData });
     }
-    catch(error){
-        res.render('error', {error: error})
+    catch (error) {
+        res.render('error', { error: error })
     }
 
 };
-  
+
 module.exports = {
     date_index,
     search_date,
--- models/data_model.js
+++ models/data_model.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 
 
-const callApi = async (date) => {
+const callNasaApi = async (date) => {
     try {
-        console.log("call API called")
+        console.log("call nasa API called")
         const api_key = "eOUQWfv4Hiq8j4eQbJbyyVjEQqPfuaXGyEx6U28S"
         const baseUrl = "https://api.nasa.gov/planetary/apod?api_key="+api_key
         const mainUrl = baseUrl + "&date=" +date
-        console.log("calling api url: ", mainUrl)
+        console.log("calling nasa api url: ", mainUrl)
 
         const response = await axios.get(mainUrl);
 
@@ -17,7 +17,21 @@ const callApi = async (date) => {
     }
 }
 
+const callMlApi = async (month, day) => {
+    try {
+        console.log("call ml API called")
+        const baseUrl = "http://history.muffinlabs.com/date/"+month+"/"+day
+        console.log("calling ml api url: ", baseUrl)
+
+        const response = await axios.get(baseUrl);
+
+        return response.data;
+    } catch (error) {
+      throw error;
+    }
+}
 
 module.exports = {
-    callApi
+    callNasaApi,
+    callMlApi
 };
